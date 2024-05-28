ae2195036da9b55f3e72fd31b31139387ccf27a2
ChocoboDancer <kat.stanley@gmail.com>
2024-05-27 23:02:35 -0400

    Started inventory display

---
+++ GDTV2024/scripts/utilities/inventories.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func list_items(items):
+	for i in items:
+		get_node("ItemList").add_item("item name","image link")
